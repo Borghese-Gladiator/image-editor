06120fcb398b28010c9eda2ba4fa6e0561952f59
Timothy Shee <timothy_shee@hotmail.com>
2020-11-22 17:44:17 -0500

    Center EditPage text

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: "center"
+  },
+}));
+
 export default function EditPage(props) {
+  const classes = useStyles();
   const { a } = props;
   return (
-    <Container>
-      <h2 style={{textAlign: "center"}}>Image Editor</h2>
+    <Container className={classes.root}>
+      <h2>Image Editor</h2>
     </Container>
   )
 }
