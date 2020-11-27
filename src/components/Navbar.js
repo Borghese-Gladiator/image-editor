ecbd07049ae41c5a18dc91a71843b1e7f1c17899
Timothy Shee <timothy_shee@hotmail.com>
2020-11-26 23:39:33 -0500

    Refactored & Fixed navbar to have close button in top right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,15 @@ import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
+
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import CancelIcon from '@material-ui/icons/Cancel';
+
+// custom components
+import BackdropStepper from './BackdropStepper';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
@@ -23,46 +31,61 @@ const useStyles = makeStyles((theme) => ({
   },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
   },
+  negativeMarginUp: {
+    marginTop: -40
+  }
 }));
 
 export default function PrimaryNavbar(props) {
   const classes = useStyles();
   const { logo } = props;
   const [open, setOpen] = React.useState(false);
+  
   const handleClose = () => {
     setOpen(false);
   };
   const handleToggle = () => {
     setOpen(!open);
+    console.log(open);
   };
   
   return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar className={classes.flexCenter}>
-          
-          <div className={classes.grow} />
-          <Button 
-            aria-label={"back to home image"}
-            color="inherit"
-          >
-            <Typography variant="h6" display="block" gutterBottom>
-              {""}
-            </Typography>
-            <img src={logo} className={classes.logo} alt="logo" />
-          </Button>
-          
-          <div className={classes.grow} />
-          <Button variant="contained" color="secondary" onClick={handleToggle} >
-            HOW TO USE
-          </Button>
-        </Toolbar>
-        <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      </AppBar>
+    <div>
+        <AppBar position="static">
+          <Toolbar className={classes.flexCenter}>
+            <div className={classes.grow} />
+            <Button 
+              aria-label={"back to home image"}
+              color="inherit"
+            >
+              <Typography variant="h6" display="block" gutterBottom>
+                {""}
+              </Typography>
+              <img src={logo} className={classes.logo} alt="logo" />
+            </Button>
+            
+            <div className={classes.grow} />
+
+            <ClickAwayListener onClickAway={handleClose}>
+              <div>
+                <Button variant="contained" color="secondary" onClick={handleToggle} >
+                  HOW TO USE
+                </Button>
+                {open ? (
+                  <Backdrop className={classes.backdrop} open={open}>
+                    <Container style={{display: "flex"}}>
+                      <BackdropStepper />
+                      <Fab color="secondary" aria-label="add" onClick={handleClose} className={classes.negativeMarginUp}>
+                        <CancelIcon fontSize="large" />
+                      </Fab>
+                    </Container>
+                  </Backdrop>
+                ) : null}
+              </div>
+            </ClickAwayListener>
+          </Toolbar>
+        </AppBar>
     </div>
   );
 }
